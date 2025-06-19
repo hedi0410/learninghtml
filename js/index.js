113b3ee8d57f27e4439d1163baf8c31f2eeadd23
hedi0410 <h.boubecker@gmail.com>
2025-06-19 14:48:24 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(20, 20, 75, 50);
+
+//Turn transparency on
+ctx.globalAlpha = 0.2;
+ctx.fillStyle = "blue";
+ctx.fillRect(50, 50, 75, 50);
+ctx.fillStyle = "green";
+ctx.fillRect(80, 80, 75, 50);
